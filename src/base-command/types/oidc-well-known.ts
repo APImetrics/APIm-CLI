6d105168dcf4e54a575b3dd1f34842e57b5c8502
Matthew Nickson <matthew@apimetrics.com>
2023-12-12 19:55:26 +0000

    Fixed linting errors
    Signed-off-by: Matthew Nickson <matthew@apimetrics.com>

--- src/base-command/types/oidc-well-known.ts
+++ src/base-command/types/oidc-well-known.ts
@@ -1,25 +1,24 @@
-/* eslint-disable camelcase */
 export type OIDCWellKnown = {
-  issuer: string;
   authorization_endpoint: string;
-  token_endpoint: string;
+  backchannel_logout_session_supported: boolean;
+  backchannel_logout_supported: boolean;
+  claims_supported: string[];
+  code_challenge_methods_supported: string[];
   device_authorization_endpoint: string;
-  userinfo_endpoint: string;
-  mfa_challenge_endpoint: string;
+  id_token_signing_alg_values_supported: string[];
+  issuer: string;
   jwks_uri: string;
+  mfa_challenge_endpoint: string;
   registration_endpoint: string;
+  request_parameter_supported: boolean;
+  request_uri_parameter_supported: boolean;
+  response_modes_supported: string[];
+  response_types_supported: string[];
   revocation_endpoint: string;
   scopes_supported: string[];
-  response_types_supported: string[];
-  code_challenge_methods_supported: string[];
-  response_modes_supported: string[];
   subject_types_supported: string[];
-  id_token_signing_alg_values_supported: string[];
+  token_endpoint: string;
   token_endpoint_auth_methods_supported: string[];
-  claims_supported: string[];
-  request_uri_parameter_supported: boolean;
-  request_parameter_supported: boolean;
   token_endpoint_auth_signing_alg_values_supported: string[];
-  backchannel_logout_supported: boolean;
-  backchannel_logout_session_supported: boolean;
+  userinfo_endpoint: string;
 };
